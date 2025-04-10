552c195d950c1721e61290de5ee3c322bb857792
Farhan Raza <razafarhan967@gmail.com>
2025-04-11 02:21:38 +0500

    Update and rename pattern.cpp to Quadrilateral pattern.cpp

---
+++ Quadrilateral pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int m;
+cout<<"Enter number of rows: "; 
+cin>> m;
+cout<<"Enter number of columns: ";
+int n;
+cin>> n;
+for(int i=1; i<=m; i++){     
+    for(int i=1; i<=n; i++){ 
+    cout<<"* ";              
+    }
+    cout<<endl;
+}
+
+
+
+
+
+
